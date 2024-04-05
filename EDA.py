9ba46d3e011526ac7d9ec5178294886231bf2f0b
Alpha7-7 <55599369+Alpha7-7@users.noreply.github.com>
2024-04-04 23:38:37 -0700

    go

--- EDA.py
+++ EDA.py
@@ -2,10 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Read the train data into a pandas DataFrame
-train_data = pd.read_csv('train.csv')
-
-# Display the first few rows of the dataset
+# Read the data into a pandas DataFrame
+train_data = pd.read_csv('/Users/AlecChen/Documents/GitHub/Kaggle-DS-Projects/house-prices-advanced-regression-techniques/train.csv')
+test_data = pd.read_csv('/Users/AlecChen/Documents/GitHub/Kaggle-DS-Projects/house-prices-advanced-regression-techniques/test.csv')
+## EDA Display the first few rows of the dataset
 print(train_data.head())
 
 # Get the summary statistics of the dataset
@@ -13,3 +13,4 @@ print(train_data.describe())
 
 # Check for missing values
 print(train_data.isnull().sum())
+print(train_data.columns)
